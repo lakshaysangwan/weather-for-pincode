657808fbdae87cca62c1b24f6fa99f98b063b9c5
Lakshay Sangwan <63947451+lakshaysangwan@users.noreply.github.com>
2023-10-25 21:06:09 +0530

    Nothing major.

--- src/main/java/com/lakshay/weatherapi/entity/UniqueWeatherRecord.java
+++ src/main/java/com/lakshay/weatherapi/entity/UniqueWeatherRecord.java
@@ -14,7 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class UniqueWeatherRecord {
     @Id
-    private long id;
+    private String id;
     private int pincode;
     private String date;
     private String apiResponse;
